7b10e1005f4dba935be0a39aa1993ecf4381f2a7
Winter Alava <walava@espol.edu.ec>
2020-09-11 12:37:07 -0500

    Programacion de MainActivity y creacion de ResultActivity

--- app/src/main/java/com/grupo1/amst/theheroproject/MainActivity.java
+++ app/src/main/java/com/grupo1/amst/theheroproject/MainActivity.java
@@ -3,12 +3,18 @@ package com.grupo1.amst.theheroproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText busqueda;
+    String nombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        busqueda = findViewById(R.id.txtBusqueda);
+        nombre = busqueda.getText().toString();
+        // buscarHeroe(nombre)
     }
 }
