2c304de0f5c85d94546cc6eafd1dbcdd1623e204
Winter Alava <walava@espol.edu.ec>
2020-09-11 12:16:39 -0500

    Primer commit - creacion de proyecto

---
+++ app/src/main/java/com/grupo1/amst/theheroproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.grupo1.amst.theheroproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
